9f3763f8a4959cab4a615fdba83a5c0b72acc815
Greice Lacerda <greicelacerda@gmail.com>
2026-01-31 01:45:19 -0300

    Meu primeiro registro matemático

---
+++ calculo.py
@@ -0,0 +1,22 @@
+# No Python, usamos o '#' para fazer comentários que não são executados. 
+# É o lugar ideal para suas anotações pedagógicas!
+
+soma = 15 + 10                  # Adição padrão
+subtracao = 50 - 25             # Subtração padrão
+multiplicacao = 7 * 8           # Usamos o asterisco (*) para multiplicar
+divisao = 10 / 3                # A barra (/) resulta em um número decimal
+divisao_inteira = 10 // 3       # A barra dupla (//) descarta as casas decimais
+resto_divisao = 10 % 3          # O símbolo de porcentagem (%) nos dá o resto
+potencia = 2 ** 3               # Dois asteriscos (**) indicam exponenciação (2 elevado a 3)
+raiz_quadrada = 16 ** 0.5       # Elevamos a 0.5 para obter a raiz quadrada
+
+# Podemos imprimir os resultados para ver os valores
+print("\n")
+print(f"O resultado da soma é: {soma}\n")
+print(f"O resultado da subtração é: {subtracao}\n")
+print(f"O resultado da multiplicação é: {multiplicacao}\n")
+print(f"O resultado da divisão é: {divisao}\n")
+print(f"O resultado da divisão inteira é: {divisao_inteira}\n")
+print(f"O resto da divisão é: {resto_divisao}\n")
+print(f"O resultado da potência é: {potencia}\n")
+print(f"A raiz quadrada de 16 é: {raiz_quadrada}\n")
---
+++ media_escolar.py
@@ -0,0 +1,11 @@
+# Passo 1: Definindo os valores (como as variáveis de uma equação)
+nota1 = 8.5
+nota2 = 7.0
+nota3 = 9.0
+nota4 = 6.5
+
+# Passo 2: O cálculo da média (respeitando a precedência aritmética)
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Passo 3: Exibindo o resultado de forma elegante
+print(f"\nA média das notas é: {media:.1f}\n")
